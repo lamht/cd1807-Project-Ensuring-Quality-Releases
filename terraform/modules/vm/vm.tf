dc01c4c1dd6d8a057d171d34d7eab6852d259a45
Lam Huynh <thanhlam0290@gmail.com>
2024-03-17 10:34:22 -0600

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,26 +1,30 @@
-resource "azurerm_network_interface" "" {
-  name                = "vm_network1"
-  location            = "eastus"
-  resource_group_name = "Azuredevops"
+resource "azurerm_network_interface" "test" {
+  name                = "NIC-${var.resource_type}-${var.application_type}"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "vm_subnet_1"
+    subnet_id                     = var.public_subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = "lamht_vm1"
-  location            = "eastus"
-  resource_group_name = "Azuredevops"
-  size                = "Standard_DS2_v2"
-  admin_username      = "devopsagent"
-  network_interface_ids = []
+resource "azurerm_linux_virtual_machine" "test" {
+  name                  = "${var.resource_type}-${var.application_type}"
+  location              = var.location
+  resource_group_name   = var.resource_group
+  size                  = "Basic_A1"
+  admin_username        = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.test.id
+  ]
+  
   admin_ssh_key {
-    username   = "evopsagent"
-    public_key = "file("~/.ssh/id_rsa.pub")"
+    username   = var.admin_username
+    public_key = file("/home/vsts/work/_temp/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
@@ -29,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 }
