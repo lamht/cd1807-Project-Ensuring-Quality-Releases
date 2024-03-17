34e58ce94707ff9941b8d04b02308159cd33f574
Lam Huynh <thanhlam0290@gmail.com>
2024-03-17 09:51:14 -0600

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,10 +1,7 @@
 provider "azurerm" {
-  tenant_id       = "${var.tenant_id}"
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
   features {}
 }
+
 terraform {
   backend "azurerm" {
     storage_account_name = ""
@@ -13,6 +10,7 @@ terraform {
     access_key           = ""
   }
 }
+
 module "resource_group" {
   source               = "../../modules/resource_group"
   resource_group       = "${var.resource_group}"
