f0ee9c26294624dd985d1eb6dfa4687887e8c5bb
Lam Huynh <thanhlam0290@gmail.com>
2024-03-17 11:33:35 -0600

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -11,6 +11,12 @@ terraform {
   }
 }
 
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = var.resource_group
+  location             = var.location
+}
+
 
 module "network" {
   source               = "../../modules/network"
